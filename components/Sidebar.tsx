c8f6000e5b0cb6a83acb119168bcad12742e3ae1
NENWA618 <sylvestertian@gmail.com>
2026-02-19 02:04:41 +0800

    fix: silence unused-var warnings in Sidebar types

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,14 +4,14 @@ interface Props {
   categories: string[];
   stats: Record<string, number>;
   onNewNote: () => void;
-  onSearch: (value: string) => void;
-  onCategoryChange: (value: string) => void;
-  onSortChange: (value: string) => void;
+  onSearch: (v: string) => void;
+  onCategoryChange: (v: string) => void;
+  onSortChange: (v: string) => void;
   searchValue: string;
   selectedCategory: string;
   sortBy: string;
   onExport?: () => void;
-  onImport?: (file: File) => void;
+  onImport?: (f: File) => void;
   onClearAll?: () => void;
 }
 
