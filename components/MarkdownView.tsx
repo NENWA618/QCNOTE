0a04e2a18d169a9bf87962d76b5ba50886076205
NENWA618 <sylvestertian@gmail.com>
2026-02-26 19:20:22 +0800

    feat(markdown): add MarkdownView component with rehype-sanitize (add dependency)

---
+++ components/MarkdownView.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeSanitize from 'rehype-sanitize';
+
+interface Props {
+  source: string;
+  className?: string;
+}
+
+const MarkdownView: React.FC<Props> = ({ source, className }) => {
+  return (
+    <div className={className}>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeSanitize]}>
+        {source}
+      </ReactMarkdown>
+    </div>
+  );
+};
+
+export default MarkdownView;
